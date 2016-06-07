de12157fb3db4909967e3963aef9e64b627aa70b
Dedi ikan Mulyana <dedi.mulyana@gmail.com>
2016-06-07 12:34:19 +0700

    ewtwrta

--- testRepository/ViewController.swift
+++ testRepository/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("sms finance")
+        print("sms finance")
+        print("sms finance")
+        
     }
 
     override func didReceiveMemoryWarning() {
